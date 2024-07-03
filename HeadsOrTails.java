43c50a7c2286ac636bc5ada33f6a9453e1ded3f7
Saku0207 <s1310123@u-aizu.ac.jp>
2024-07-04 01:00:23 +0900

    HoT: initial revision

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -1,1 +1,24 @@
-a
+import  java.util.Random;
+
+public class HeadsOrTails{
+    public static void main(String[] args) {
+        System.err.println("Tossing a coin...");
+
+        Random random = new Random();
+        int head = 0;
+        int tail = 0;
+
+        for(int i = 1; i<= 3;i++){
+           boolean Heads = random.nextBoolean();
+           if(Heads){
+            System.err.println("Round " + i+ ": Heads");
+            head++;
+           }else{
+            System.err.println("Round "+i+ ": Tails");
+            tail++;
+           }
+         }
+         System.err.println("Heads: " + head+ ", Tails: " + tail);
+
+        }
+    }
